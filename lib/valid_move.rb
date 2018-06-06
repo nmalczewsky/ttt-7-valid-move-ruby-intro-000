55deb8bf898418a254240052c8805032250a8218
Nicholas Malczewsky <n.malczewsky@gmail.com>
2018-06-06 16:16:54 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,9 +2,7 @@
 def valid_move? (board, index)
   if position_taken?(board, index)
     return false
-  elsif board[index] != between(0, 8)
-    return false
-  else
+  elsif board[index] == between(0, 8)
     return true
   end
 end
